02709fe2faa94ac37b8d33553f4f08ee64323b3f
mahardikalgw <muhammadkharismam@gmail.com>
2025-05-22 13:56:02 +0800

    create function

--- contracts/4_ProposalContract.sol
+++ contracts/4_ProposalContract.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract ProposalContract {
 
+    uint256 private counter;
     struct Proposal {
         string title;               // New title field added
         string description;
@@ -16,5 +17,10 @@ contract ProposalContract {
     }
 
     mapping(uint256 => Proposal) proposal_history;
+
+    function create(string calldata _description, uint256 _total_vote_to_end) external {
+        counter += 1;
+        proposal_history[counter] = Proposal("title",_description, 0, 0, 0, _total_vote_to_end, false, true);
+    }
 
 }
